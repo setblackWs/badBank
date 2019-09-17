35ad7df2cd75c8bcedf796628ab81fb88b8a405e
Andreas Doebeli <thobens@gmail.com>
2019-09-17 22:16:19 +0200

    Add Test for Account according to specs in Readme.
    The test deposit_NegativeAmount_throwsIllegalArgumentException should fail because it
    does not implement the test mentioned in the Readme that a negative amount cannot be deposited

---
+++ src/main/test/ch/engenius/bank/BankBuilder.java
@@ -0,0 +1,4 @@
+package ch.engenius.bank;
+
+public class BankBuilder {
+}
---
+++ src/main/test/ch/engenius/bank/BankTest.java
@@ -0,0 +1,18 @@
+package ch.engenius.bank;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BankTest {
+    @Test
+    public void registerAccount() throws Exception {
+
+    }
+
+    @Test
+    public void getAccount() throws Exception {
+
+    }
+
+}
